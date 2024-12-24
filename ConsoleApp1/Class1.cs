9d9203c768535386945c42c656f14f54dd766787
315 <315@315-28>
2024-12-24 11:39:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeamLibrary
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Player> Players { get; set; }
+
+        public Team()
+        {
+            Players = new List<Player>();
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Team Name: {Name}");
+            Console.WriteLine($"Team Address: {Address}");
+            Console.WriteLine("Players:");
+            foreach (var player in Players)
+            {
+                player.Show();
+            }
+        }
+
+        public void ShowAll()
+        {
+            Players.Sort((p1, p2) => p1.Name.CompareTo(p2.Name));
+            foreach (var player in Players)
+            {
+                player.Show();
+            }
+        }
+
+        public void AddPlayer(Player player)
+        {
+            Players.Add(player);
+        }
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Player(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"- {Name}: {Description}");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using TeamLibrary;
+
+namespace TeamApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Team team = new Team();
+            team.Name = "Golden Dragons";
+            team.Address = "123 Main Street";
+
+
+            team.AddPlayer(new Player("John Doe", "Forward"));
+            team.AddPlayer(new Player("Jane Smith", "Goalkeeper"));
+            team.AddPlayer(new Player("Bob Johnson", "Midfielder"));
+
+            // Шеченков Андрей 34 ис
+            team.ShowAll();
+            Console.ReadLine();
+        }
+    }
+}
